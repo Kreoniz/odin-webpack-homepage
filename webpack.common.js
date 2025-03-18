5d2dcce33e782764f44d6d73d33d4f5114853665
Kreoniz <kreoniz@mail.ru>
2025-03-18 18:35:18 +0600

    chore(deps): add and configure html webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,6 +10,11 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
